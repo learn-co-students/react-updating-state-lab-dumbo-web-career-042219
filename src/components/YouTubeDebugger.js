26a2f6af6ed6a09c6fbb75182e6a479ca0717d28
Aaron Chu <aaronchu357@gmail.com>
2019-06-25 23:00:06 -0400

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -29,7 +29,7 @@ export default class YouTubeDebugger extends Component {
       settings: {
         ...this.state.settings,
         video: {
-          ...this.state.settings.video,
+          // ...this.state.settings.video,
           resolution: '720p'
         }
       }
